83ee58504c1ad303c82a884b5f6ea8b202315ed4
rafael <rafael.zoe26@gmail.com>
2024-03-19 05:41:36 -0300

    feat: create entities

---
+++ src/RocketseatAuction.Api/Entities/Auction.cs
@@ -0,0 +1,9 @@
+﻿namespace RocketseatAuction.Api.Entities;
+
+public class Auction
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public DateTime Starts { get; set; }
+    public DateTime Ends { get; set; }
+}
