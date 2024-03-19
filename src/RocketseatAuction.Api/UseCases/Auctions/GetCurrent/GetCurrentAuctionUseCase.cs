d963eee46a64ab51e94ad2dd5deb80bf9902e7e1
rafael <rafael.zoe26@gmail.com>
2024-03-19 05:42:36 -0300

    feat: create useCases

---
+++ src/RocketseatAuction.Api/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -0,0 +1,17 @@
+﻿using RocketseatAuction.Api.Entities;
+using RocketseatAuction.Api.Repositories;
+
+namespace RocketseatAuction.Api.UseCases.Auctions.GetCurrent;
+
+public class GetCurrentAuctionUseCase
+{
+    public Auction Execute()
+    {
+
+        var repository = new RocketseatAuctionDbContext();
+
+        var auction = repository.Auctions.First();
+
+        return auction;
+    }
+}
